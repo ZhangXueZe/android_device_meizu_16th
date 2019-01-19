4cc37ab980aeb1d62b3625e78ee789faa9f3b550
CoderStory <coderstory@163.com>
2019-01-19 13:50:38 +0800

    Update lineage_dipper.mk

--- lineage_dipper.mk
+++ lineage_dipper.mk
@@ -10,17 +10,17 @@ $(call inherit-product, device/xiaomi/dipper/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_dipper
-PRODUCT_DEVICE := dipper
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Mi 8
-PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := lineage_16th
+PRODUCT_DEVICE := 16th
+PRODUCT_BRAND := meizu
+PRODUCT_MODEL := 16th
+PRODUCT_MANUFACTURER := meizu
 
 BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.11.0.OEACNFA:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEACNFA release-keys" \
-    PRODUCT_NAME="dipper" \
-    TARGET_DEVICE="dipper"
+    PRODUCT_NAME="16th" \
+    TARGET_DEVICE="16th"
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-meizu
